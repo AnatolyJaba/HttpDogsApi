8e7005d933545eb6e49f42ab11e3da7b40a44adc
Tolik <Tolik@JABA-ANATOLY>
2023-04-06 00:02:50 +0300

    Добавьте файлы проекта.

---
+++ HttpDogsApi/Controllers/DogsApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpDogsApi.Controllers
+{
+	[Route("httpdog/{file}")]
+	[ApiController]
+	public class DogsApiController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get(string file)
+		{
+			Byte[] image = System.IO.File.ReadAllBytes(@$"wwwroot\img\{file}");        
+			return File(image, "image/jpeg");
+		}
+	}
+}
---
+++ HttpDogsApi/Controllers/DogsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HttpDogsApi.Controllers
+{
+	public class DogsController : Controller
+	{
+		public IActionResult Index(int code)
+		{
+			string baseString = $"https://localhost:7257/httpdog/{code}.jpg";
+			return View((object)baseString);
+		}
+	}
+}
